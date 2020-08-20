fefdad12ecab4c5905d4a1d421b52d18f2c23398
elizabethcook21 <43394196+elizabethcook21@users.noreply.github.com>
2020-08-20 15:03:16 -0600

    deleted all files  Please enter the commit message for your changes. Lines starting

--- voe/tests/testthat/test-voe_pipeline.R
+++
@@ -1,9 +0,0 @@
-# This is a modified test for a small abundance file
-test_that("small sample voe pipeline works", {
-  metadata <- readRDS('~/OneDrive/Harvard SIBMI/voe/data/final_metadata.rds')
-  abundance <- readRDS('~/OneDrive/Harvard SIBMI/voe/data/abundance_data.rds')
-  abundance <- abundance[1:100]
-  mapping <- readRDS('~/OneDrive/Harvard SIBMI/voe/data/Karlsson_Mapping.rds')
-  mapping <- mapping %>% head(100)
-  expect_error(voepipeline(metadata, abundance, mapping), regexp = NA)
-})

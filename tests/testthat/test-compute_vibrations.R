cdb03b666d775d8ffaa9288569b2085a6e615583
Braden Tierney <bradentierney@Bradens-MacBook-Pro.local>
2020-08-26 11:00:31 -0700

    cleaning and cli

--- tests/testthat/test-compute_vibrations.R
+++ tests/testthat/test-compute_vibrations.R
@@ -1,6 +1,6 @@
 test_that("vibrations function appropriately", {
-	metadata = readRDS('../../data/final_metadata.rds')[[2]][[1]] %>% type.convert() %>% dplyr::select(-runID,-subjectID)
-	abundance = readRDS('../../data/abundance_data.rds') %>% dplyr::select(-dataset_name)
+	metadata = readRDS('../../data/metadata_for_test.rds')[[2]][[1]]
+	abundance = readRDS('../../data/abundance_data_for_test.rds') 
 	abundance = abundance[,1:10]
 	bound_data = dplyr::tibble(dependent_variables=list(abundance),independent_variables=list(metadata),dsid=1)
   	expect_error(compute_vibrations(bound_data=bound_data,primary_variable='BMI',model_type='gaussian',features_of_interest=c("2","3","4","5"),max_vibration_num=10,proportion_cutoff=.9), regexp = NA)
--- tests/testthat/test-voe_pipeline.R
+++ tests/testthat/test-voe_pipeline.R
@@ -1,13 +1,13 @@
 test_that("non-meta-analytic pipeline works", {
-	metadata = readRDS('../../data/final_metadata.rds')[[2]][[1]] %>% type.convert() %>% dplyr::select(-runID,-subjectID)
-	abundance = readRDS('../../data/abundance_data.rds') %>% dplyr::select(-dataset_name)
+	metadata = readRDS('../../data/metadata_for_test.rds')[[2]][[1]]
+	abundance = readRDS('../../data/abundance_data_for_test.rds') 
 	abundance = abundance[,1:100]
   	expect_error(full_voe_pipeline(abundance,metadata,primary_variable='BMI',fdr_method='BY',model_type='gaussian',fdr_cutoff=0.99,max_vibration_num=10,proportion_cutoff = 0.9, meta_analysis=FALSE), regexp = NA)
 })
 
 test_that("meta-analytic pipeline works", {
-	metadata = readRDS('../../data/final_metadata.rds')[[2]][[1]] %>% type.convert() %>% dplyr::select(-runID,-subjectID)
-	abundance = readRDS('../../data/abundance_data.rds') %>% dplyr::select(-dataset_name)
+	metadata = readRDS('../../data/metadata_for_test.rds')[[2]][[1]] 
+	abundance = readRDS('../../data/abundance_data_for_test.rds')
 	abundance = abundance[,1:100]
   	expect_error(full_voe_pipeline(list('dataset1'=abundance,'dataset2'=abundance),list('dataset1'=metadata,'dataset2'=metadata),model_type='gaussian',primary_variable='BMI',fdr_method='BY',fdr_cutoff=0.99,max_vibration_num=10,proportion_cutoff = 0.9, meta_analysis=TRUE), regexp = NA)
 })

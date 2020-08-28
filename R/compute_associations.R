b75ce2bd2eaf3f32d4e52d7010100a7a67dc020a
Braden Tierney <bradentierney@Bradens-MacBook-Pro.local>
2020-08-28 10:30:37 -0700

    update

--- R/compute_associations.R
+++ R/compute_associations.R
@@ -40,7 +40,9 @@ run_associations <- function(x,primary_variable,model_type,proportion_cutoff,vib
 }
 
 compute_initial_associations <- function(bound_data,primary_variable, model_type, proportion_cutoff,vibrate){
-    output = apply(bound_data, 1, function(x) run_associations(x,primary_variable,model_type,proportion_cutoff))
+    output = apply(bound_data, 1, function(x) run_associations(x,primary_variable,model_type,proportion_cutoff,vibrate))
     output[['output']] = dplyr::bind_rows(output[['output']])
+    print(output[['output']])
+    print(output[['vibrate']])
   return(output)
 }

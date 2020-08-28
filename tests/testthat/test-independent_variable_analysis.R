16cd13b491cbc20d8318ac15202ec8beef34bb83
elizabethcook21 <43394196+elizabethcook21@users.noreply.github.com>
2020-08-28 15:18:58 -0600

    more updates

--- tests/testthat/test-independent_variable_analysis.R
+++ tests/testthat/test-independent_variable_analysis.R
@@ -1,10 +1,6 @@
 test_that("metadata analysis works", {
   metadata <- readRDS('~/OneDrive/Harvard SIBMI/voe/data/metadata_for_test.rds')
   metadata <- metadata[2:length(metadata)]
-  metadata$LDL <- as.numeric(metadata$LDL)
-  metadata$TGL <- as.numeric(metadata$TGL)
-  metadata$HDL <- as.numeric(metadata$HDL)
-  metadata$BMI <- as.numeric(metadata$BMI)
-  metadata$age <- as.numeric(metadata$age)
   expect_error(ind_var_analysis(metadata), regexp = NA)
+  ind_var_analysis(metadata)
 })

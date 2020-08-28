fb1ad88408306b6881db82541191fc0f2718de80
Braden Tierney <bradentierney@Bradens-MacBook-Pro.local>
2020-08-27 20:16:24 -0700

    debug vibration

--- voe_command_line_deployment.R
+++ voe_command_line_deployment.R
@@ -43,7 +43,7 @@ message('Parsing input data...')
 dependent_variable_locs = strsplit(as.character(opt$dependent_variables),',')
 dependent_variables=list()
 
-print(length(unlist(dependent_variable_locs))>)
+print(length(unlist(dependent_variable_locs))) 
 
 if(length(unlist(dependent_variable_locs))>1){
 	for(i in unlist(dependent_variable_locs)){

dbdf2d0e3973a798a9e1f13879ffe1a6f649410d
Braden Tierney <bradentierney@Bradens-MacBook-Pro.local>
2020-08-27 17:37:41 -0700

    debug vibration

--- voe_command_line_deployment.R
+++ voe_command_line_deployment.R
@@ -47,7 +47,7 @@ print(length(dependent_variable_locs[[1]]))
 if(length(dependent_variable_locs[[1]])>1){
 	for(i in seq_along(dependent_variable_locs[[1]]))
     print(trimws(dependent_variable_locs[[1]][[i]]))
-    data = tibble:as_tibble(readRDS(trimws(dependent_variable_locs[[1]][[i]])))
+    data = readRDS(trimws(dependent_variable_locs[[1]][[i]]))
     colnames(data)[1]='sampleID'
     print('here')
 		dependent_variables[[i]] = data

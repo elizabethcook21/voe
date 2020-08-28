71aa1e4d8097f00023e98e424ffcb33ac403ea53
Braden Tierney <bradentierney@Bradens-MacBook-Pro.local>
2020-08-27 17:50:02 -0700

    debug vibration

--- voe_command_line_deployment.R
+++ voe_command_line_deployment.R
@@ -61,7 +61,7 @@ if(length(independent_variable_locs[[1]])>1){
 	for(i in seq_along(independent_variable_locs[[1]]))
   print((independent_variable_locs[[1]][[i]]))
 		data = tibble:as_tibble(readRDS(trimws(independent_variable_locs[[1]][[i]])))
-    #colnames(data)[1]='sampleID'
+    colnames(data)[1]='sampleID'
     #independent_variables[[i]]=data 
 }
 if(length(independent_variable_locs[[1]])==1){

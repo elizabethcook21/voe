847a4e06a18293f1dc10aee94ab93ae45d27c168
Braden Tierney <bradentierney@Bradens-MacBook-Pro.local>
2020-08-28 11:00:57 -0700

    update

--- R/summarize_linear_output.R
+++ R/summarize_linear_output.R
@@ -19,7 +19,7 @@ find_confounders_linear <- function(voe_list_for_reg){
   voe_adjust_for_reg_ptype <- voe_list_for_reg %>% dplyr::select_if(~ length(unique(.)) > 1) %>% dplyr::select(-c(full_fits,std.error,statistic))
   voe_adjust_for_reg_ptype$estimate=abs(voe_adjust_for_reg_ptype$estimate)
   print(unlist(unname(table(voe_adjust_for_reg_ptype$independent_feature))))
-  if('independent_feature' %in% colnames(voe_adjust_for_reg_ptype) and !(1 %in% unlist(unname(table(voe_adjust_for_reg_ptype$independent_feature))))){
+  if('independent_feature' %in% colnames(voe_adjust_for_reg_ptype) & !(1 %in% unlist(unname(table(voe_adjust_for_reg_ptype$independent_feature))))){
     fit_estimate=lme4::lmer(data=voe_adjust_for_reg_ptype,as.formula(estimate ~ . +(1|independent_feature) -independent_feature - estimate - p.value),control = lme4::lmerControl(optimizer = "bobyqa"))
     fit_estimate_forplot=broom.mixed::tidy(fit_estimate) %>% dplyr::mutate(sdmin=(estimate-std.error),sdmax=(estimate+std.error))
   }

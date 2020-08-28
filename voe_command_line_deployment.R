5ca242c81ac0260a1a6d821de333266ffd443030
Braden Tierney <bradentierney@Bradens-MacBook-Pro.local>
2020-08-27 17:28:51 -0700

    debug vibration

--- voe_command_line_deployment.R
+++ voe_command_line_deployment.R
@@ -45,9 +45,8 @@ dependent_variables=list()
 
 if(length(dependent_variable_locs[[1]])>1){
 	for(i in seq_along(dependent_variable_locs[[1]]))
-    data = tibble:tibble(readRDS(trimws(dependent_variable_locs[[1]][[i]])))
+    data = tibble:as_tibble(readRDS(trimws(dependent_variable_locs[[1]][[i]])))
     colnames(data)[1]='sampleID'
-    print(head(data))
 		dependent_variables[[i]] = data
 }
 if(length(dependent_variable_locs[[1]])==1){
@@ -59,9 +58,8 @@ independent_variable_locs = strsplit(opt$independent_variables,',')
 independent_variables=list()
 if(length(independent_variable_locs[[1]])>1){
 	for(i in seq_along(independent_variable_locs[[1]]))
-		data = tibble:tibble(readRDS(trimws(independent_variable_locs[[1]][[i]])))
+		data = tibble:as_tibble(readRDS(trimws(independent_variable_locs[[1]][[i]])))
     colnames(data)[1]='sampleID'
-    print(head(data))
     independent_variables[[i]]=data 
 }
 if(length(independent_variable_locs[[1]])==1){

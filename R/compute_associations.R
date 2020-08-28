0415f61400c9bf9f65e08cdb7fbf325f4bd896e0
Braden Tierney <bradentierney@Bradens-MacBook-Pro.local>
2020-08-28 10:46:02 -0700

    update

--- R/compute_associations.R
+++ R/compute_associations.R
@@ -42,7 +42,8 @@ compute_initial_associations <- function(bound_data,primary_variable, model_type
     output = apply(bound_data, 1, function(x) run_associations(x,primary_variable,model_type,proportion_cutoff,vibrate))
     output_regs = map(output, function(x) x[[1]])
     output_vib = unique(map(output, function(x) x[[2]]))
-    if(FALSE %in% output_vib){
+    if(FALSE %in% output_vib & vibrate!=FALSE){
+      output_vib=FALSE
      message('For at least one dataset, we dropped all the variables that you could possible vibrate over due to NAs or lacking multiple levels. Vibrate parameter being set to FALSE.')
     }
     output_regs = dplyr::bind_rows(output_regs)

05ba9461b970056c313d75f4304f79f2875e4f60
elizabethcook21 <43394196+elizabethcook21@users.noreply.github.com>
2020-08-28 23:35:19 -0600

    updates on the metadata creater

--- R/independent_variable_analysis.R
+++ R/independent_variable_analysis.R
@@ -2,32 +2,36 @@
 #'
 #' This function will run the full pipeline
 #' #' @param independent_variables A tibble containing the information for your independent variables (e.g. age, sex). Each column should correspond to a different variable (e.g. age), with the first column containing the sample names matching those in the column anmes of the dependent_variables tibble.
+#' #' @param output_location The path where you want to save all the ggplots and metadata
 #' @keywords independent variable
 #' @export
 #' @examples
 #' ind_var_analysis(metadata)
-ind_var_analysis <- function(independent_variables){
+ind_var_analysis <- function(independent_variables, output_location = getwd()){
   my_logger <- initialize_logger(paste0('ind_var_analysis_',format(Sys.time(), "%d-%b-%Y_%H.%M")))
+
+  # Create a folder to store all the outputs
+  pathToNewFolder <- paste0(output_location, '/Metadata_Files')
+  dir.create(pathToNewFolder)
+
   ind_var <- dplyr::as_tibble(independent_variables)
   columnSummaries <- summary(ind_var)
   # This should probably be changed in the future to a more human readable file type like txt, html, pdf, or excel
-  saveRDS(columnSummaries, 'metadata_information.rds')
+  saveRDS(columnSummaries, paste0(pathToNewFolder, '/metadata_information.rds'))
   log4r::info(my_logger, 'You should have gotten a summary of all the columns in your table, for example their means and number of variables.')
   # Get the different types of columns by using summarise_all, then convert to a named list
-  browser()
-
   colTypes <- ind_var %>%
     dplyr::summarise_all(class)
   colTypes <- as.list(colTypes)
   for(i in 1:length(colTypes)){
-    #columnOfData <- ind_var[names(colTypes[i])
-    if(colTypes[i] == "factor"){
+    if (nrow(unique(ind_var[i])) > 10){
+      histogram_tibble <- dplyr::as_tibble(ind_var[names(colTypes[i])])
       try(
-        makeBarGraph(ind_var, names(colTypes[i]))
+        makeHistogram(histogram_tibble, names(colTypes[i]),pathToNewFolder)
       )
-    } else {
+    }else{
       try(
-        makeHistogram(ind_var[names(colTypes[i])], names(colTypes[i]))
+        makeBarGraph(ind_var, names(colTypes[i]),pathToNewFolder)
       )
     }
   }
@@ -35,16 +39,21 @@ ind_var_analysis <- function(independent_variables){
 }
 
 
-makeBarGraph <- function(table, columnName){
+makeBarGraph <- function(table, columnName,pathToNewFolder){
   ggplot2::ggplot(table, ggplot2::aes_string(x=columnName, fill = columnName)) +
     ggplot2::geom_bar() +
-    ggplot2::theme_light()
-  try(ggplot2::ggsave(paste0(columnName,"_bargraph.png")))
+    ggplot2::theme_light() +
+    ggplot2::ggtitle(paste0(columnName, ' Comparison')) +
+    ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
+  ggplot2::ggsave(filename = file.path(pathToNewFolder,paste0(columnName, "_bargraph.png")))
+
 }
 
-makeHistogram <- function(table, columnName){
-  ggplot2::ggplot(table, ggplot2::aes(x=columnName)) +
+makeHistogram <- function(table, columnName,pathToNewFolder){
+  ggplot2::ggplot(table, ggplot2::aes_string(x=columnName)) +
     ggplot2::geom_histogram() +
-    ggplot2::theme_light()
-  try(ggplot2::ggsave(paste0(columnName, "_histogram.png")))
+    ggplot2::theme_light() +
+    ggplot2::ggtitle(paste0(columnName, ' Distribution')) +
+    ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
+  ggplot2::ggsave(filename = file.path(pathToNewFolder,paste0(columnName, "_histogram.png")))
 }
--- tests/testthat/test-independent_variable_analysis.R
+++ tests/testthat/test-independent_variable_analysis.R
@@ -1,5 +1,5 @@
 test_that("metadata analysis works", {
-  metadata <- readRDS('~/OneDrive/Harvard SIBMI/voe/data/metadata_for_test.rds')
+  metadata <- readRDS('../../data/metadata_for_test.rds')
   metadata <- metadata[2:length(metadata)]
   testthat::expect_error(ind_var_analysis(metadata), regexp = NA)
   ind_var_analysis(metadata)

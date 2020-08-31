fc75e2d80e202f6bea491f38718ec004d6ca1d7e
elizabethcook21 <43394196+elizabethcook21@users.noreply.github.com>
2020-08-31 12:33:05 -0600

    added conditional logging argument

--- R/initialize_logger.R
+++ R/initialize_logger.R
@@ -1,13 +1,20 @@
-initialize_logger <- function(fileName){
+initialize_logger <- function(fileName, saveLog){
 
-  my_logfile = paste0('logfile_',fileName, '.txt')
-
-  my_console_appender = log4r::console_appender()
-  my_file_appender = log4r::file_appender(my_logfile, append = TRUE)
-
-  my_logger <- log4r::logger(threshold = "INFO",
-                             appenders= list(my_console_appender,my_file_appender))
-
-  return(my_logger)
+  if(saveLog == TRUE){
+    my_logfile = paste0('logfile_',fileName, '.txt')
+    
+    my_console_appender = log4r::console_appender()
+    my_file_appender = log4r::file_appender(my_logfile, append = TRUE)
+    
+    my_logger <- log4r::logger(threshold = "INFO",
+                               appenders= list(my_console_appender,my_file_appender))
+    
+    return(my_logger)
+  }else{
+    my_console_appender = log4r::console_appender()
+    my_logger <- log4r::logger(threshold = "INFO",
+                               appenders= list(my_console_appender))
+  }
+ 
 
 }
--- R/voe_pipeline.R
+++ R/voe_pipeline.R
@@ -16,8 +16,8 @@
 #' @export
 #' @examples
 #' voepipeline(metadata, abundance_data, mapping)
-full_voe_pipeline <- function(dependent_variables,independent_variables,primary_variable,vibrate=TRUE,fdr_method='BY',fdr_cutoff=0.05,max_vibration_num=50000,proportion_cutoff=.95,meta_analysis=FALSE, model_type='gaussian'){
-  logger <- initialize_logger(paste0('voe_pipeline_',format(Sys.time(), "%d-%b-%Y_%H.%M")))
+full_voe_pipeline <- function(dependent_variables,independent_variables,primary_variable,vibrate=TRUE,fdr_method='BY',fdr_cutoff=0.05,max_vibration_num=50000,proportion_cutoff=.95,meta_analysis=FALSE, model_type='gaussian',log=FALSE){
+  logger <- initialize_logger(paste0('voe_pipeline_',format(Sys.time(), "%d-%b-%Y_%H.%M")), log)
   output_to_return = list()
   if(inherits(dependent_variables, "list")==TRUE){
     log4r::info(logger,'Identified multiple input datasets, preparing to run meta-analysis.')

cc8ab9f73911916994df16a0e161745886a59578
Braden Tierney <bradentierney@Bradens-MacBook-Pro.local>
2020-09-03 15:35:27 -0700

    new parameters

--- voe_command_line_deployment.R
+++ voe_command_line_deployment.R
@@ -12,7 +12,9 @@ spec = matrix(c(
   'fdr_method', 'm', 1, "character","Multiple hypothesis testing method (BY, BH, bonferroni, default = BY)",
   'fdr_cutoff', 'c', 1, "double","Float between 0.0 and 1.0, FDR-adjusted p-value threshold. (default = 0.05)",
   'max_vibration_num', 'n', 1, "integer","Integer, max number of vibrations per feature. (default = 50000)",
-  'proportion_cutoff', 'p', 1, "double","Float between 0.0 and 1.0. (default = 0.9",
+  'max_vars_in_model', 'r', 1, "integer","Integer, max number of independent variables in vibration. (default = NULL)",
+  'confounder_analysis', 'y', 1, "logical","TRUE/FALSE -- mixed effects analysis to identify sources of confounding in dataset (default=TRUE).",
+  'proportion_cutoff', 'g', 1, "double","Float between 0.0 and 1.0. (default = 0.9",
   'meta_analysis', 'a', 1, "logical","TRUE/FALSE (default: FALSE)",
   'cores', 't', 1, "integer","Number of cores to use (default = 1).",
   'model_type', 'f', 1, "character","GLM family. (default: gaussian)",
@@ -25,9 +27,11 @@ if ( !is.null(opt$help) ) {
   cat(getopt(spec, usage=TRUE))
   q(status=1)
 }
-
+max_vibration_num = opt$max_vibration_num
 ### set defaults if necessary
 if ( is.null(opt$fdr_method    ) ) { opt$fdr_method    = 'BY'     }
+if ( is.null(opt$max_vars_in_model    ) ) { opt$max_vars_in_model    = NULL     }
+if ( is.null(opt$confounder_analysis    ) ) { opt$confounder_analysis    = TRUE     }
 if ( is.null(opt$cores    ) ) { opt$cores    = 1     }
 if ( is.null(opt$vibrate    ) ) { opt$vibrate    = TRUE     }
 if ( is.null(opt$fdr_cutoff      ) ) { opt$fdr_cutoff      = 0.05     }
@@ -76,9 +80,10 @@ if(length(unlist(independent_variable_locs))==1){
 
 message('Data parsed and loaded, running pipeline.')
 
-output = voe::full_voe_pipeline(dependent_variables=dependent_variables,independent_variables=independent_variables,primary_variable=opt$primary_variable,fdr_method=opt$fdr_method,fdr_cutoff=opt$fdr_cutoff,max_vibration_num=opt$max_vibration_num,proportion_cutoff=opt$proportion_cutoff,meta_analysis=opt$meta_analysis, model_type=opt$model_type,cores=opt$cores)
+output = voe::full_voe_pipeline(dependent_variables=dependent_variables,independent_variables=independent_variables,primary_variable=opt$primary_variable,fdr_method=opt$fdr_method,confounder_analysis=opt$confounder_analysis,fdr_cutoff=opt$fdr_cutoff,max_vibration_num=opt$max_vibration_num,proportion_cutoff=opt$proportion_cutoff,meta_analysis=opt$meta_analysis,max_vars_in_model = opt$max_vars_in_model, model_type=opt$model_type,cores=opt$cores)
 
 if(exists("output")==TRUE){
+  message('Writing output to file...')
   saveRDS(output,opt$output_path)
 }
 
